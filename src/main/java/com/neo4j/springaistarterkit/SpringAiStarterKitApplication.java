a50289df64ff7ecd1ef3d0390bbded3d9225e4eb
Jennifer Reif <jennifer.reif@neo4j.com>
2024-02-27 13:10:41 -0600

    Initial commit

---
+++ src/main/java/com/neo4j/springaistarterkit/SpringAiStarterKitApplication.java
@@ -0,0 +1,13 @@
+package com.neo4j.springaistarterkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiStarterKitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiStarterKitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neo4j/springaistarterkit/SpringAiStarterKitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neo4j.springaistarterkit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiStarterKitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
